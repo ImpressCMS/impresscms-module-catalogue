ffeeb29bc496704fdcd2cd8eec6320703dc541bd
Simon <simon@isengard.biz>
2012-02-22 11:56:52 +0000

    Mark as V1.1 final.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/catalogue/trunk@23665 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/catalogue/icms_version.php
+++ modules/catalogue/icms_version.php
@@ -33,8 +33,8 @@ $modversion = array(
 
 /**  Development information */
   'status_version'=> "1.1",
-  'status'=> "Beta",
-  'date'=> "8/2/2012",
+  'status'=> "Final",
+  'date'=> "22/2/2012",
   'author_word'=> "Live long and prosper.",
 
 /** Contributors */
