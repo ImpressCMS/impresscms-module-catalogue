71669f77e16ad7ea7246537fc0456e458e96c953
Simon <simon@isengard.biz>
2013-01-16 04:36:09 +0000

    Filing Catalogue 1.13. final.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/catalogue/releases/1.13%20final@24832 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ catalogue/admin/menu.php
@@ -0,0 +1,58 @@
+<?php
+/**
+* Configuring the amdin side menu for the module
+*
+* @copyright	Copyright Madfish (Simon Wilkinson)
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+* @package		catalogue
+* @version		$Id$
+*/
+
+global $icmsConfig;
+
+$i = -1;
+
+$i++;
+$adminmenu[$i]['title'] = _MI_CATALOGUE_ITEMS;
+$adminmenu[$i]['link'] = 'admin/item.php';
+
+// Orders are not implemented yet
+//$i++;
+//$adminmenu[$i]['title'] = _MI_CATALOGUE_ORDERS;
+//$adminmenu[$i]['link'] = 'admin/order.php';
+
+$catalogueModule = icms_getModuleInfo(basename(dirname(dirname(__FILE__))));
+
+if (isset($catalogueModule)) {
+
+	$i = -1;
+
+	$i++;
+	$headermenu[$i]['title'] = _CO_ICMS_GOTOMODULE;
+	$headermenu[$i]['link'] = ICMS_URL . '/modules/catalogue/';
+	
+	$i++;
+	$headermenu[$i]['title'] = _PREFERENCES;
+	$headermenu[$i]['link'] = '../../system/admin.php?fct=preferences&amp;op=showmod&amp;mod=' .
+		$catalogueModule->getVar('mid');
+	
+	$i++;
+	$headermenu[$i]['title'] = _MI_CATALOGUE_TEMPLATES;
+	$headermenu[$i]['link'] = '../../system/admin.php?fct=tplsets&op=listtpl&tplset='
+		. $icmsConfig['template_set'] . '&moddir=' . $catalogueModule->getVar('dirname');
+
+	$i++;
+	$headermenu[$i]['title'] = _CO_ICMS_UPDATE_MODULE;
+	$headermenu[$i]['link'] = ICMS_URL . '/modules/system/admin.php?fct=modulesadmin&op=update&module='
+		. $catalogueModule->getVar('dirname');
+
+	$i++;
+	$headermenu[$i]['title'] = _MI_CATALOGUE_MANUAL;
+	$headermenu[$i]['link'] = ICMS_URL . '/modules/' . $catalogueModule->getVar('dirname') . '/manual/catalogue_manual.pdf';
+
+	$i++;
+	$headermenu[$i]['title'] = _MODABOUT_ABOUT;
+	$headermenu[$i]['link'] = ICMS_URL . '/modules/' . $catalogueModule->getVar('dirname') . '/admin/about.php';
+}
---
+++ catalogue/class/ItemHandler.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+* Class representing Catalogue item handler objects
+*
+* @copyright	Copyright Madfish (Simon Wilkinson)
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+* @package		catalogue
+* @version		$Id$
+*/
+
+class CatalogueItemHandler extends icms_ipf_Handler {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct(& $db) {
+		
+		parent::__construct($db, 'item', 'item_id', 'title', 'description',
+			'catalogue');
+
+		// enable upload of product photos, this should be handled by core mimetype manager later
+		global $catalogueConfig;
+		$mimetypes = array('image/jpeg', 'image/png', 'image/gif');
+		$this->enableUpload($mimetypes,
+			$catalogueConfig['image_file_size'],
+			$catalogueConfig['image_upload_width'],
+			$catalogueConfig['image_upload_height']);
+	}
+	
+	/** Provides global search functionality for Catalogue module
+     *
+	 */
+	public function getItemsForSearch($queryarray, $andor, $limit, $offset, $userid) {
+		$criteria = new icms_db_criteria_Compo();
+		$criteria->setStart($offset);
+		$criteria->setLimit($limit);
+		$criteria->setSort('date');
+		$criteria->setOrder('DESC');
+
+		if ($userid != 0) {
+			$criteria->add(new icms_db_criteria_Item('submitter', $userid));
+		}
+		if ($queryarray) {
+			$criteriaKeywords = new icms_db_criteria_Compo();
+			for ($i = 0; $i < count($queryarray); $i++) {
+				$criteriaKeyword = new icms_db_criteria_Compo();
+				$criteriaKeyword->add(new icms_db_criteria_Item('title', '%' . $queryarray[$i] . '%',
+					'LIKE'), 'OR');
+				$criteriaKeyword->add(new icms_db_criteria_Item('description', '%' . $queryarray[$i]
+					. '%', 'LIKE'), 'OR');
+				$criteriaKeywords->add($criteriaKeyword, $andor);
+				unset ($criteriaKeyword);
+			}
+			$criteria->add($criteriaKeywords);
+		}
+		$criteria->add(new icms_db_criteria_Item('online_status', TRUE));
+		return $this->getObjects($criteria, TRUE, FALSE);
+	}
+	
+	/**
+	 * Switches an items status from online to offline or vice versa
+	 *
+	 * @return null
+	 */
+	public function changeVisible($item_id) {
+		$visibility = '';
+		$itemObj = $this->get($item_id);
+		if ($itemObj->getVar('online_status', 'e') == TRUE) {
+			$itemObj->setVar('online_status', 0);
+			$visibility = 0;
+		} else {
+			$itemObj->setVar('online_status', 1);
+			$visibility = 1;
+		}
+		$this->insert($itemObj, TRUE);
+		return $visibility;
+
+	}
+
+	/**
+	 * Converts status value to human readable text
+	 *
+	 * @return array
+	 */
+	public function online_status_filter() {
+		return array(0 => 'Offline', 1 => 'Online');
+	}
+	
+	public function updateComments($item_id, $total_num) {
+		$itemObj = $this->get($item_id);
+		if ($itemObj && !$itemObj->isNew()) {
+			$itemObj->setVar('item_comments', $total_num);
+			$this->insert($itemObj, TRUE);
+		}
+	}
+
+	/**
+	 * Triggers notifications, called when an item is inserted or updated
+	 *
+	 * @param object $obj CatalogueItem object
+	 * @return bool
+	 */
+	protected function afterSave(& $obj) {
+		
+		$sprockets_taglink_handler = '';
+		
+		// triggers notification event for subscribers
+		if (!$obj->getVar('item_notification_sent') && $obj->getVar('online_status', 'e') == 1) {
+			$obj->sendNotifItemPublished();
+			$obj->setVar('item_notification_sent', TRUE);
+			$this->insert ($obj);
+		}
+		
+		// storing tags
+		$sprocketsModule = icms_getModuleInfo('sprockets');
+		
+		if ($_SERVER['REQUEST_METHOD'] == 'POST' && icms_get_module_status("sprockets")) {
+			$sprockets_taglink_handler = icms_getModuleHandler('taglink', 
+				$sprocketsModule->getVar('dirname'), 'sprockets');
+			$sprockets_taglink_handler->storeTagsForObject($obj, 'tag', 0);
+		}
+		
+		return TRUE;
+	}
+
+	/**
+	 * Deletes notification subscriptions, called when an item is deleted
+	 *
+	 * @global object $icmsModule
+	 * @param object $obj CatalogueItem object
+	 * @return bool
+	 */
+	protected function afterDelete(& $obj) {
+		$notification_handler = icms::handler("icms_data_notification");
+		$module = icms::handler("icms_module")->getByDirname(basename(dirname(dirname(__FILE__))));
+		$module_id = $module->getVar('mid');
+		$category = 'global';
+		$item_id = $obj->id();
+
+		// delete global notifications
+		$notification_handler->unsubscribeByItem($module_id, $category, $item_id);
+		
+		// delete taglinks
+		$sprocketsModule = icms_getModuleInfo('sprockets');
+		if (icms_get_module_status("sprockets")) {
+			$sprockets_taglink_handler = icms_getModuleHandler('taglink',
+					$sprocketsModule->getVar('dirname'), 'sprockets');
+			$sprockets_taglink_handler->deleteAllForObject(&$obj);
+		}
+
+		return TRUE;
+	}
+}
