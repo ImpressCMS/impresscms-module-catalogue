9d3f5cba6325d9eede79ff645bb897eb954cd629
Simon <simon@isengard.biz>
2013-02-20 08:09:17 +0000

    Mark as Catalogue V1.14 final.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/catalogue/trunk@24900 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/catalogue/icms_version.php
+++ modules/catalogue/icms_version.php
@@ -33,8 +33,8 @@ $modversion = array(
 
 /**  Development information */
   'status_version'=> "1.14",
-  'status'=> "Beta",
-  'date'=> "11/2/2013",
+  'status'=> "Final",
+  'date'=> "20/2/2013",
   'author_word'=> "Live long and prosper.",
 
 /** Contributors */
